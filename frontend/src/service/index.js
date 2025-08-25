49e36f0353df2aad38b98fc1cba67d56cb488f6c
Anusree <anilkumaranusree113@gmail.com>
2025-08-26 00:27:38 +0530

    Fix environment variable name and update code formatting

--- frontend/src/service/index.js
+++ frontend/src/service/index.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
 // const API_URL = "http://localhost:8082/tasks";
-const API_URL = process.env.REACT_API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const fetchTasks = async () => {
-    const response = await axios.get(API_URL);
-    return response.data;
+  const response = await axios.get(API_URL);
+  return response.data;
 };
 
 export const createTask = async (formData) => {
-    await axios.post(API_URL, formData);
+  await axios.post(API_URL, formData);
 };
 
 export const updateTask = async (taskId, taskData) => {
-    await axios.patch(`${API_URL}/${taskId}`, taskData);
+  await axios.patch(`${API_URL}/${taskId}`, taskData);
 };
 
 export const deleteTask = async (taskId) => {
-    await axios.delete(`${API_URL}/${taskId}`);
+  await axios.delete(`${API_URL}/${taskId}`);
 };
 
 export const markTaskAsDone = async (taskId) => {
-    await axios.patch(`${API_URL}/${taskId}`, { status: "DONE" });
+  await axios.patch(`${API_URL}/${taskId}`, { status: "DONE" });
 };
