c0a8d97ef784974af6f2adeb65b869302949585a
Anusree <anilkumaranusree113@gmail.com>
2025-08-26 00:09:35 +0530

    fix: Add root route redirect and update formatting

--- backend/index.js
+++ backend/index.js
@@ -5,23 +5,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/task.routes");
 
-
 const app = express();
 const PORT = process.env.PORT;
 const DB_URI = process.env.MONGO_URI;
 // const DB_URI = "mongodb://localhost:27017/task-manager";
 
 mongoose
-    .connect(DB_URI)
-    .then(() => console.log("DB Connected!"))
-    .catch((error) => console.log("Error in connecting DB", error));
+  .connect(DB_URI)
+  .then(() => console.log("DB Connected!"))
+  .catch((error) => console.log("Error in connecting DB", error));
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.redirect("/tasks");
+});
+
 app.use("/tasks", router);
 
 app.listen(PORT, () => {
-    console.log(`Backend listening on Port ${PORT}!`);
+  console.log(`Backend listening on Port ${PORT}!`);
 });
 
-module.exports = app
+module.exports = app;
